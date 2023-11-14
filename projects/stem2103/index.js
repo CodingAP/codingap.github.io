02621e6ee647b302a74c4c00c46e3d44a62d3572
CodingAP <apsoutherngaming07@gmail.com>
2023-11-14 12:40:24 -0600

    fixing it

--- projects/stem2103/index.js
+++ projects/stem2103/index.js
@@ -45,7 +45,7 @@ const margin = { x: 100, y: 50 };
 const barHeight = 30;
 const scrollingUnits = 21;
 const unitSize = 500;
-const dialog = { width: 0.66, height: 0.4 };
+const dialog = { width: 0.8, height: 0.66 };
 
 const yearLineName = index => {
     if (index > 20) {
@@ -57,16 +57,43 @@ const yearLineName = index => {
 
 const events = [
     {
-        timeline: 1.5,
-        name: 'i pooped',
+        timeline: 7.75,
+        name: 'September 2010; 3rd Grade',
         color: 'red',
-        description: 'i was pooping my pants at this age idk'
+        description:
+            `I was a smart cookie, so I was very good at those speed math tests (do 30
+problems in a minute, stuff like that). I was so good and enthralled that I decided to go all
+the way and finish the add/subtract section, followed by the multiply/divide section.
+However, a new format appeared that I couldn't recognize (I would later learn it to be the
+exponent). The question was 15^2. I started it by doing what I normally do with
+multiplication and got 30, which was wrong. I first tried to assimilate what it was before
+asking my teacher what it was, which made me have to accommodate the fact that
+exponent's was multiplication but with the number itself. The upper number indicates
+how many times I had to multiply it, so the correct answer was 225.`
     },
     {
-        timeline: 11.5,
-        name: 'i didn\'t pooped',
+        timeline: 4.75,
+        name: 'Late 2007; Preschool',
         color: 'green',
-        description: 'i was constipated!'
+        description:
+            `My event was in preschool in 2007 when I was 4 years old. We needed to clean up after
+playtime because we were messy children, but I just wanted to sing the song, no clean
+up. While walking around and singing, the other students noticed that I wasn't doing
+much. One even clued in the teacher, who didn't do much at the start, but quickly asked
+what I was doing when I sat down. Me, a very young child who didn't want to get in
+trouble, started to help, but it was too late. The play area was already clean, so there
+was nothing I could do. The ZPD comes from looking at the other students cleaning up
+and the teacher not getting on to them, so I learned cleaning up was good.`
+    },
+    {
+        timeline: 21,
+        name: 'Summary',
+        color: 'blue',
+        description:
+            `Lots of my experiences consisted of social interactions between peers or my family. Those, I
+think, were the real shapers of my life and education. Most of the time, I would learn something
+from someone, which would build off everything, and then I would “teach” it to someone, which
+made my education grow even more.`
     }
 ]
 
@@ -95,7 +122,7 @@ const loop = () => {
 
     // draw scrolling stuff
     context.save();
-    
+
     let scroll = { x: canvas.width / 2 - progress * unitSize * scrollingUnits, y: canvas.height / 2 };
     context.translate(scroll.x, scroll.y);
 
@@ -127,7 +154,7 @@ const loop = () => {
         context.closePath();
         context.fill();
 
-        context.fillText(events[i].name, events[i].timeline * unitSize, 25);
+        context.fillText(events[i].name, events[i].timeline * unitSize, -50);
 
         let translatedEventPosition = { x: eventPosition.x + scroll.x, y: eventPosition.y + scroll.y };
 
@@ -155,16 +182,19 @@ const loop = () => {
             width: dialog.width * canvas.width,
             height: dialog.height * canvas.height
         };
-        
+
         context.fillStyle = '#aaa';
         context.fillRect(description.x, description.y, description.width, description.height);
 
+        let lines = events[eventSelected].description.split('\n');
         context.fillStyle = '#000';
-        context.font = '50px Arial';
-        context.textAlign = 'center';
-        context.textBaseline = 'middle';
-        context.fillText(events[eventSelected].description, canvas.width / 2, canvas.height / 2)
-    
+        context.font = '20px Arial';
+        context.textAlign = 'left';
+        context.textBaseline = 'top';
+        for (let i = 0; i < lines.length; i++) {
+            context.fillText(lines[i], description.x + 10, description.y + 10 + (i * 20));
+        }
+
         if (!(mouse.position.x > description.x && mouse.position.x < description.x + description.width &&
             mouse.position.y > description.y && mouse.position.y < description.y + description.height) && mouse.button == 0) {
             eventSelected = -1;
